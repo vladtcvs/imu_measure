300db558239f060ab4ac8f054779dcc6d12f9583
Tsendrovskii Vladislav <vlad@vlad.vltc.net.eu.org>
2015-07-10 01:03:35 +0300

    move libfilter to this project

---
+++ filter.cc
@@ -0,0 +1,71 @@
+#include <filter.h>
+
+const double eps[3][3][3] = { { {0, 0, 0}, {0, 0, 1}, {0, -1, 0} }, { {0, 0, -1}, {0, 0, 0}, {1, 0, 0} }, { {0, 1, 0}, {-1, 0, 0}, {0, 0, 0} } };
+
+#define SQR(x) ((x)*(x))
+
+gyro_unit::gyro_unit()
+{
+	w.setZero();
+	P.setZero();
+	I.setOnes();
+}
+
+
+gyro_unit::gyro_unit(Vector3d nw, Matrix3d nP, Vector3d nI) :
+	w(nw),
+	P(nP),
+	I(nI)
+{
+}
+
+void gyro_unit::kalman_step(Vector3d z, Matrix3d Q, Matrix3d R, Vector3d M, double dt)
+{
+	Matrix3d Lambda;
+	int q, j, m, n, i, k;
+	Vector3d f(M(0)/I(0)*dt, M(1)/I(1)*dt, M(2)/I(2)*dt);
+
+	Lambda.setZero();
+	for (q = 0; q < 3; q++)
+	for (j = 0; j < 3; j++) {
+		double l = 0;
+		for (m = 0; m < 3; m++)
+		for (n = 0; n < 3; n++) {
+			l -= 2 * w(n) * P(j,m) * eps[q][m][n] * (I(n) - I(m))/(2*I(q)) * dt;
+			l -= 2 * w(n) * P(q,m) * eps[j][m][n] * (I(n) - I(m))/(2*I(j)) * dt;
+		}
+		Lambda(q,j) = l;
+	}
+	Lambda += P + R;
+	Matrix3d LLt = (Lambda + Lambda.transpose());
+	Matrix3d QL = 2*Q + LLt;
+	Matrix3d K = LLt * (QL.inverse());
+
+	Vector3d wc = w + f;
+	for (i = 0; i < 3; i++) {
+		double dw = 0;
+		for (j = 0; j < 3; j++)
+		for (k = 0; k < 3; k++)
+			dw -= eps[i][j][k]*(I(k)-I(j))/(2*I(i))*dt*w(j)*w(k);
+		wc(i) += dw;
+	}
+
+	Matrix3d E;
+	for (i = 0; i < 3; i++)
+	for (j = 0; j < 3; j++)
+		E(i,j) = (i == j);
+
+	Vector3d FP;
+	FP.setZero();
+	for (j = 0; j < 3; j++) {
+		for (m = 0; m < 3; m++)
+		for (n = 0; n < 3; n++)
+			FP(j) -= P(m,n) * eps[j][m][n]*(I(n)-I(m))/(2*I(j))*dt;
+	}
+
+	Matrix3d T = (E-K);
+	Vector3d ws = T*wc + K*z + T * FP;
+	Matrix3d Ps = T * Lambda * (T.transpose()) + K * Q * (K.transpose());
+	w = ws;
+	P = Ps;
+}
---
+++ filter.h
@@ -0,0 +1,23 @@
+#ifndef __FILTER_H__
+#define __FILTER_H__
+
+#include <Eigen/Dense>
+
+using namespace Eigen;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+class gyro_unit {
+	Vector3d I;
+public:
+	Vector3d w;
+	Matrix3d P;
+
+	gyro_unit();
+	gyro_unit(Vector3d nw, Matrix3d nP, Vector3d nI);
+	void kalman_step(Vector3d z, Matrix3d Q, Matrix3d R, Vector3d M, double dt);
+};
+
+extern const double eps[3][3][3];
+
+#endif

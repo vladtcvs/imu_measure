084ee5201c3274e7e21eb141c37a6a14ad26b7b2
Tsendrovskii Vladislav <vlad@vlad.vltc.net.eu.org>
2015-07-25 14:30:06 +0300

    control

--- read_config.cc
+++ read_config.cc
@@ -50,17 +50,37 @@ std::map<std::string, std::string> parse_config(std::string cfg)
 double parse_double(std::string value)
 {
 	double d = 0;
+	std::istringstream i(value);
+	if (!(i >> d))
+		throw 1;
 	return d;
 }
 
 Eigen::Vector3d parse_vector3(std::string value)
 {
 	Eigen::Vector3d vec;
+	int j;
+	std::istringstream i(value);
+	for (j = 0; j < 3; j++) {
+		double d;
+		if (!(i >> d))
+			throw 1;
+		vec(j) = d;
+	}
 	return vec;
 }
 
 Eigen::Matrix3d parse_matrix3(std::string value)
 {
-	Eigen::Matrix3d vec;
-	return vec;
+	int j, k;
+	Eigen::Matrix3d mat;
+	std::istringstream i(value);
+	for (j = 0; j < 3; j++)
+	for (k = 0; k < 3; k++) {
+		double d;
+		if (!(i >> d))
+			throw 1;
+		mat(j, k) = d;
+	}
+	return mat;
 }
